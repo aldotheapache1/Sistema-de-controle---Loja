d4cd83676ad3d2373c852edc9ed22a04fd9e4ae4
Gian Lucas <gian15249@gmail.com>
2021-01-13 16:12:28 -0300

    index table

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,11 @@
 
         <div class="info-topo">
             <a class="nome-usuario" href="{{ route('profile.show') }}">{{ Auth::user()->name }}</a>
-            <p class="caixa-fechado">Caixa Aberto/Fechado</p>
+            @if(count($currentCashRegister) > 0)
+                <p class="caixa-aberto">Caixa Aberto</p>
+            @else
+                <p class="caixa-fechado">Caixa Fechado</p>
+            @endif
         </div>
 
         <nav class="aside-menu">
@@ -65,35 +69,100 @@
             </a>
         </div>
         @if(count($currentCashRegister) > 0)
-            <table class="table_index">
-                <thead>
-                    <tr>
-                        <th>Produto</th>
-                        <th>Tipo Venda</th>
-                        <th>Vendedor</th>
-                        <th>Cliente</th>
-                        <th>Qtd</th>
-                        <th>Valor</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>      
-                        <td>2</td>      
-                        <td>3</td>      
-                        <td>4</td>      
-                        <td>5</td>      
-                        <td>6</td>      
-                    </tr>
-                </tbody>
+            <div class="checkboxes">
+                <input type="checkbox" id="payments" name="payments">
+                <label for="payments">Pagamentos</label>
 
-                    <tr>
-                        <td>Total em vendas: R$ {{$currentCashRegister['initial_value']}},00</td>      
-                        <td>Total em caixa: R$ {{$currentCashRegister['initial_value']}},00 </td>      
-                           
-                    </tr>
+                <input type="checkbox" id="sales" name="sales" checked>
+                <label for="sales">Vendas</label>
+            </div>
+            
+            <div class="table-index">
+                <table >
+                    <thead>
+                        <tr>
+                            <th>Produto</th>
+                            <th>Tipo Venda</th>
+                            <th>Vendedor</th>
+                            <th>Cliente</th>
+                            <th>Qtd</th>
+                            <th>Valor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Camisa azul Lacoste</td>      
+                            <td>À vista</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>5</td>      
+                            <td>R$ 500,00</td>      
+                        </tr>
+                        <tr>
+                            <td>Camisa azul Lacoste</td>      
+                            <td>À vista</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>5</td>      
+                            <td>R$ 500,00</td>      
+                        </tr>
+                        <tr>
+                            <td>Camisa azul Lacoste</td>      
+                            <td>À vista</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>5</td>      
+                            <td>R$ 500,00</td>      
+                        </tr>
+                        <tr>
+                            <td>Camisa azul Lacoste</td>      
+                            <td>À vista</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>5</td>      
+                            <td>R$ 500,00</td>      
+                        </tr>
+                        <tr>
+                            <td>Camisa azul Lacoste</td>      
+                            <td>À vista</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>5</td>      
+                            <td>R$ 500,00</td>      
+                        </tr>
+                        <tr>
+                            <td>Camisa azul Lacoste</td>      
+                            <td>À vista</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>5</td>      
+                            <td>R$ 500,00</td>      
+                        </tr>
+                        <tr>
+                            <td>Camisa azul Lacoste</td>      
+                            <td>À vista</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>5</td>      
+                            <td>R$ 500,00</td>      
+                        </tr>
+                        <tr>
+                            <td>Camisa azul Lacoste</td>      
+                            <td>À vista</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>Gian Lucas da Silva Ramalho</td>      
+                            <td>5</td>      
+                            <td>R$ 500,00</td>      
+                        </tr>
+                        <tr>
+                            <td class="table_total" colspan="3">Total em vendas: R$ {{$currentCashRegister['initial_value']}},00</td>      
+                            <td class="table_total" colspan="3">Total em caixa: R$ {{$currentCashRegister['initial_value']}},00 <span>Vendas á vista(50,00) Pagamentos (50,00)</span></td>      
+                        </tr>
+                    </tbody>
 
-            </table>
+                </table>
+            </div>
+            
         @else
             <div class="form-valor-caixa">
                 <form action="/index" method="POST">
